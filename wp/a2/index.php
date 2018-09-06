41d1962b57a5a485a783f77ae76c951e2e192c69
Lam Tran <s3714110@student.rmit.edu.au>
2018-09-06 19:07:56 +1000

    djdj

---
+++ wp/a2/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>Assignment 2</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <script src='../wireframe.js'></script>
+  </head>
+
+  <body>
+    <header>
+    <img src='../../media/MSBStone-logo.png' = alt ='Melbourne Stone Benchtops'/>
+
+    <nav class= "cool">
+      <ul>
+        <li><a href=index.php>Home</a></li>
+        <li><a href=services.php>Services</a></li>
+        <li><a href=login.php>Members</a></li>
+      </ul>
+    </nav>
+  </header>
+    <main>
+    <div class ="l-wrap">
+      <section class="three-col-grid">
+        <article class="grid-item">
+          <p>About Us!<p>
+            <p>moretext</p>
+            <p>About Us!<p>
+              <p>moretext</p>
+              <p>About Us!<p>
+                <p>moretext</p>
+        </article>
+        <article class="grid-item"><img src='../../media/MSBStone-img(3).jpg' width="330" height="230" alt= "Stonebench with plate of poached eggs on toast."></article>
+        <article class="grid-item"><img src='../../media/MSBStone-img(3).jpg' width="330" height="230" alt= "Stonebench with plate of poached eggs on toast."></article>
+        <article class="grid-item"><p>About Us!<p>
+          <p>moretext</p>
+          <p>About Us!<p>
+            <p>moretext</p>
+            <p>About Us!<p>
+              <p>moretext</p>
+            </article>
+      </section>
+
+
+  </div>
+</main>
+
+    <footer>
+      <div>&copy;<script>
+        document.write(new Date().getFullYear());
+      </script> Duy Linh Nguyen, s3429599.</div>
+      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </footer>
+
+
+  </body>
+
+
+
+</html>
---
+++ wp/a2/login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>Assignment 2</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <script src='../wireframe.js'></script>
+  </head>
+
+  <body>
+    <header>
+    <img src='../../media/MSBStone-logo.png' = alt ='Melbourne Stone Benchtops'/>
+
+    <nav class= "cool">
+      <ul>
+        <li><a href=index.php>Home</a></li>
+        <li><a href=services.php>Services</a></li>
+        <li><a href=login.php>Members</a></li>
+      </ul>
+    </nav>
+  </header>
+    <main>
+    <div class ="l-wrap">
+      <section class="three-col-grid">
+
+        <article class="grid-item">LOGIN</article>
+
+      </section>
+
+
+  </div>
+</main>
+
+    <footer>
+      <div>&copy;<script>
+        document.write(new Date().getFullYear());
+      </script> Duy Linh Nguyen, s3429599.</div>
+      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </footer>
+
+
+  </body>
+
+
+
+</html>
