97f95307cd0c62e081ae502adff897bff72acd10
Lam Tran <s3714110@student.rmit.edu.au>
2018-09-07 20:57:08 +1000

    8:57 PM 7/9

--- wp/a2/service.php
+++ wp/a2/service.php
@@ -30,30 +30,36 @@
         <article class="grid-content">
           <form action="https://titan.csit.rmit.edu.au/~e54061/wp/processing.php" method="post" onsubmit="return submitValue();">
             <table>
-              <tr><td>Product Name: </td>
+              <tr><td class="servicetd">Product Name: </td>
                 <td class="alignRight">Kitchen Stone</td></tr>
              <input type="text" value="kitchenstone" id="id" name="id" hidden>
 
-             <tr><td><label for="option">Color: </label></td>
-              <td class="alignRight"><select id="option" name="option" class="alignRight">
-          <option value="eminentwhitepearl" class="alignRight">Eminent White Pearl</option>
-          <option value="liquidplatinum" class="alignRight">Liquid Platinum</option>
-          <option value="nebulagraypearl" class="alignRight">Nebula Gray Pearl</option>
+             <tr><td class="servicetd"><label for="option">Color: </label></td>
+              <td class="alignRight"><select id="option" name="option" style="font-style: italic;">
+          <option value="eminentwhitepearl" >Eminent White Pearl</option>
+          <option value="liquidplatinum" >Liquid Platinum</option>
+          <option value="nebulagraypearl">Nebula Gray Pearl</option>
         </select></td></tr>
-        <tr><td>Quantity: </td>
+        <tr><td class="servicetd">Quantity: </td>
     <td class="alignRight">
 	<input type="button" class="qtybutton" value="-" onclick="minus()">
    <label for="qty"><input type="text" class="qtyfield" value="1" id="qty" name="qty"></label>
   <input type="button" class="qtybutton" value="+" onclick="plus()"></td></tr>
-            <tr><td>Price: </td>
+            <tr><td class="servicetd">Price: </td>
             <td class="alignRight">$300.00 (for a standard kitchen)</td></tr>
 
-            <tr><td><input type="submit" value="Add to cart"></td></tr>
+            <tr><td></td>
+              <td class="allignRight">
+              <input type="submit" value="Add to cart" class="submitButton"></td></tr>
         </table>
         </form>
         <script src="script.js"></script>
         </article>
-        <article class="grid-content"><img src='../../media/MSBStone-img(3).jpg' width="330" height="230" alt= "Stonebench with plate of poached eggs on toast."></article>
+        <article class="grid-content"><img src='../../media/MSBStone-img(3).jpg' width="330" height="230" alt= "Stonebench with Eminent White Pearl color"></article>
+        <article class="grid-content">
+        <span class="descriptionText"><b>Description: </b> Crafted from one of the most precious marble stones under the artistic hands of our crafting master, this kitchen bench model will lighten up your family kitchen with a bright and shiny color.</span>
+        </article>
+
       </section>
 
 
