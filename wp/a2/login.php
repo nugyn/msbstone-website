3c3e0e2d9a26f8e0e4dcb3c2affa528a9744041f
nugyn <s3429599@student.rmit.edu.au>
2018-09-07 18:29:15 +1000

    Changed the login
    margins, and styling

--- wp/a2/login.php
+++ wp/a2/login.php
@@ -29,9 +29,6 @@
       <section class="grid">
         <div class ="login">
         <form action="processing.php">
-
-
-
   <div class="container">
     <label for="uname"><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
@@ -44,7 +41,7 @@
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
   </div>
-  
+
 
   <div class="container" style="background-color:#f1f1f1">
     <button type="button" class="cancelbtn">Cancel</button>
