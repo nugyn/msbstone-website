a0664190a143d8ddfaa73cd8fe1273c3b2215d40
nugyn <s3429599@student.rmit.edu.au>
2018-09-07 20:46:18 +1000

    Login
    Spacing,
    
    location of remember me
    
    border-radius of form container and input forms

--- wp/a2/login.php
+++ wp/a2/login.php
@@ -30,20 +30,20 @@
         <div class ="login">
         <form action="processing.php">
   <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+    <label for="uname"></label>
+    <input type="text" placeholder="Username" name="uname" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <button type="submit">Login</button>
+    <label for="psw"></label>
+    <input type="password" placeholder="Password" name="psw" required>
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
+    <button type="submit">Login</button>
+    
   </div>
 
 
-  <div class="container" >
+  <div class="container">
     <button type="button" class="cancelbtn">Cancel</button>
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
